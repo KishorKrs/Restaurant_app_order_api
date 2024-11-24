b3355e5ca4626fedfd5bc7cb7c57b506fd05d9e7
Kishor <>
2024-11-24 13:11:18 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use actix_web::{web, App, HttpServer, HttpResponse, Responder};
+use serde::{Deserialize, Serialize};
+use sqlx::{FromRow, SqlitePool};
+
+fn main() {
+    println!("Hello, world!");
+}
