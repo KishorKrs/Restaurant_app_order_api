d232c948411193f9dbce896c0dbf85302c8f7985
Kishor <>
2024-11-24 13:17:18 +0900

    added Order struct for order table

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,16 @@ use actix_web::{web, App, HttpServer, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, SqlitePool};
 
+// Order struct to resemble order table
+#[derive(Serialize, Deserialize, FromRow)]
+struct Order {
+    id: Option<i64>,    // Auto-incrementing ID
+    table_number: i32,  // Table number
+    item: String,       // Name of the menu item
+    cook_time: i32,     // Time to cook item (in minutes)
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
